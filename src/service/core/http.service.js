49b4a8b191b4a6f8aa64d8bc6cb08e0c45eb3884
shivank-dev <sharmashivank59@gmail.com>
2021-02-07 03:44:47 +0530

    http service created

---
+++ src/service/core/http.service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+
+export const HTTP = axios.create({
+    baseUrl: baseUrl
+})
+
+HTTP.interceptor.response.use(undefined, (error) => {
+    if (error.message === 'Network Error' && !error.response) {
+        return toast.error('Network Error');
+    }
+    const { status } = error.response;
+
+    if (status === 404) {
+        return toast.error("404 | Not Found");
+    }
+
+    if (status === 500) {
+        return toast.error("500 | Internal Server Error");
+    }
+}
+})
