7f87e500f953c7ca27eec2df8936386031971f54
shivank-dev <sharmashivank59@gmail.com>
2021-02-07 09:52:18 +0530

    url config created

---
+++ src/config/urlConfig.js
@@ -0,0 +1,11 @@
+
+const baseUrlDev = 'http://localhost:5000';
+// const baseUrlDev = '' TODO: Host server on somewhere
+
+
+export const urls = {
+    addTodo: "/addTodo",
+    getAllTodo: "/getAllTodo",
+    updateTodo: "/updateTodo"
+}
+
